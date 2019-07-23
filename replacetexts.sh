858229fae884141967101549183b5e4fa7786dc1
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-07-23 17:32:44 +0530

    Update replacetexts.sh

--- replacetexts.sh
+++ replacetexts.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sed -i 's|"ng serve"|"ng serve --public-host=0.0.0.0/'${CONTAINERPATH}'_op/sockjs-node --host 0.0.0.0 --port 4200 --disable-host-check"|g' /home/project/$PROJECTDIR/package.json
 sed -i 's|base href="/"|base href=""|g' /home/project/$PROJECTDIR/src/index.html
+
