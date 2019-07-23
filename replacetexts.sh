14f977f8ada3fd85ecdda44b9a3dff935d9ea48f
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-07-23 23:51:37 +0530

    Update replacetexts.sh

--- replacetexts.sh
+++ replacetexts.sh
@@ -2,5 +2,7 @@
 sed -i 's|"ng serve|"ng serve --base-href='' --public-host=0.0.0.0/'${CONTAINERPATH}'_op/sockjs-node --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
 sed -i 's|base href="/"|base href=""|g' /home/project/$PROJECTDIR/src/index.html
 sed -i 's|src="./bundle.js"|src="http://d9n2pyny5ybdw.cloudfront.net/testimages/bundle.js"|g' /home/theia/lib/index.html
+wget -q -O - http://www.skillstack.com/api/onlinetests/getconfig?testid=$TESTID > /home/project/$PROJECTDIR/.pmainfile
+
 
 
