cf4e17eae7097906ee740da3a47074e6762274b5
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-07-25 22:03:10 +0530

    Update replacetexts.sh
    added --poll option because recompile was not working some cases after  file edit

--- replacetexts.sh
+++ replacetexts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i 's|"ng serve|"ng serve  --public-host=0.0.0.0/'${CONTAINERPATH}'_op/sockjs-node --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
+sed -i 's|"ng serve|"ng serve   --poll=2000  --public-host=0.0.0.0/'${CONTAINERPATH}'_op/sockjs-node --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
 sed -i 's|base href="/"|base href="/'${CONTAINERPATH}'_op/"|g' /home/project/$PROJECTDIR/src/index.html
 sed -i 's|src="./bundle.js"|src="http://d9n2pyny5ybdw.cloudfront.net/testimages/bundle.js"|g' /home/theia/lib/index.html
 wget -q -O - http://www.skillstack.com/api/onlinetests/getconfig?testid=$TESTID > /home/project/$PROJECTDIR/.pmainfile
