7859bf367d2eab69ab6c267056ce5839b218934d
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-07-26 14:16:39 +0530

    Update execbeforegitpull.sh
    should be before to avoid 'sh ng command not found'
    npm start should start after copying all node files

--- execbeforegitpull.sh
+++ execbeforegitpull.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-echo "na"
+cd /home/project/$PROJECTDIR
+echo "download tar"
+wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
+tar -xvf op.tar > /dev/null
+echo "extracted"
+rm op.tar
+
 
 
