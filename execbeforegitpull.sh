fe003cc36aa65c3b4a308e898ab3f4de49266c78
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-09-11 15:19:44 +0530

    Update execbeforegitpull.sh

--- execbeforegitpull.sh
+++ execbeforegitpull.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
 cd /home/project/$PROJECTDIR
 echo "download tar"
 wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
