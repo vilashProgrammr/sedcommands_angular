3034768badb145c3cc5c1790f9c74efbfef1a86f
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-09-13 14:17:07 +0530

    revert back

--- execbeforegitpull.sh
+++ execbeforegitpull.sh
@@ -2,10 +2,10 @@
 echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
 cd /home/project/$PROJECTDIR
 echo "download tar"
-###wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
-###tar -xvf op.tar > /dev/null
+wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
+tar -xvf op.tar > /dev/null
 echo "extracted"
-###rm op.tar
+rm op.tar
 
 
 
