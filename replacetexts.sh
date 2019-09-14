5fc6b1a9ac9c0c4aeff48504b805f24e8535dfa2
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-09-14 13:50:08 +0530

    Update replacetexts.sh

--- replacetexts.sh
+++ replacetexts.sh
@@ -4,7 +4,7 @@ sed -i 's|"ng serve|"ng serve   --poll=2000  --host 0.0.0.0 --port 4200 --disabl
 # uncomment if using reverse proxy: sed -i 's|base href="/"|base href="/'${CONTAINERPATH}'_op/"|g' /home/project/$PROJECTDIR/src/index.html
 sed -i 's|src="./bundle.js"|src="http://d9n2pyny5ybdw.cloudfront.net/testimages/bundle.js"|g' /home/theia/lib/index.html
 #wget -q -O - http://www.skillstack.com/api/onlinetests/getconfig?testid=$TESTID > /home/project/$PROJECTDIR/.pmainfile
-echo '{"mainfile":"src/index.js","terminalcommand":"wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar && tar -xvf op.tar > /dev/null && npm start"}' > /home/project/$PROJECTDIR/.pmainfile
+echo '{"mainfile":"src/index.js","terminalcommand":"npm start"}' > /home/project/$PROJECTDIR/.pmainfile
 
 
 
