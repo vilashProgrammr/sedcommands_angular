ff6579469dd5443ce9221646706a1f1f52365dbd
vilashProgrammr <31532136+vilashProgrammr@users.noreply.github.com>
2019-09-21 20:22:19 +0530

    Update replacetexts.sh

--- replacetexts.sh
+++ replacetexts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # use this if using reverse proxy: sed -i 's|"ng serve|"ng serve   --poll=2000  --public-host=0.0.0.0/'${CONTAINERPATH}'_op/sockjs-node --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
-sed -i 's|"ng serve|"ng serve   --poll=2000  --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
+sed -i 's|"ng serve"|"ng serve   --poll=2000  --host 0.0.0.0 --port 4200 --disable-host-check"|g' /home/project/$PROJECTDIR/package.json
 sed -i 's|"react-scripts start"|"PORT=4200 HOST=0.0.0.0 react-scripts start"|g' /home/project/$PROJECTDIR/package.json
 
 # uncomment if using reverse proxy: sed -i 's|base href="/"|base href="/'${CONTAINERPATH}'_op/"|g' /home/project/$PROJECTDIR/src/index.html
